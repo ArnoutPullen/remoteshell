1560b33135d2927baf6c541cca319cbd40cb57d7
ArnoutPullen <ArnoutPullen@users.noreply.github.com>
2018-05-28 01:28:50 +0200

    x

--- index.js
+++ index.js
@@ -2,6 +2,20 @@ const express = require('express');
 const app = express();
 const exec = require('child_process').exec;
 
+app.use(express.json());
+app.use(express.urlencoded({extended: true}));
+
+function response(error, response, stderror) {
+	if (stdout.includes("\n")) {
+		stdout = stdout.split("\n");
+	}
+	return {
+		"error": error,
+		"response": response,
+		"stderr": stderr
+	};
+}
+
 app.get('/', (req, res) => {
 	res.send('hello');
 });
@@ -12,31 +26,40 @@ app.get('/api/files', (req, res) => {
 		query += req.query.folder;
 	}
 	exec(query, (err, stdout, stderr) => {
-		res.json({
-			"err": err,
-			"stdout": stdout,
-			"stderr": stderr
-		});
+		if (stdout.includes("\n")) {
+			stdout = stdout.split("\n");
+		}
+		res.json(response(err, stdout, stderr));
 	});
 });
 
 app.get('/api/files/:folder', (req, res) => {
 	exec('ls /' + req.query.folder, (err, stdout, stderr) => {
-		res.json({
-			"err": err,
-			"stdout": stdout,
-			"stderr": stderr
-		});
+		res.json(response(err, stdout, stderr));
+	});
+});
+
+app.post('/api/files/create', (req, res) => {
+	var query = '';
+	if(req.body.type == 'folder') {
+		query += 'mkdir ';
+	} else if(req.body.type == 'file') {
+		query += 'touch ';
+	}
+	if(req.body.folder != null) {
+		query += `/${req.body.folder}/`;
+	}
+	if(req.body.name != null) {
+		query += req.body.name;
+	}
+	exec(query, (err, stdout, stderr) => {
+		res.json(response(err, stdout, stderr));
 	});
 });
 
 app.get('/api/custom', (req, res) => {
 	exec(req.query.command, (err, stdout, stderr) => {
-		res.json({
-			"err": err,
-			"stdout": stdout,
-			"stderr": stderr
-		});
+		res.json(response(err, stdout, stderr));
 	});
 });
 
