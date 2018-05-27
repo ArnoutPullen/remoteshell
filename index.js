d0b391109cc775815723532e1b3b4f92ba66c317
arnoutpullen <arnoutpullen@hotmail.com>
2018-05-27 20:33:06 +0000

    init

---
+++ index.js
@@ -0,0 +1,48 @@
+const express = require('express');
+const app = express();
+const exec = require('child_process').exec;
+
+app.get('/', (req, res) => {
+	exec('ls /', (err, stdout, stderr) => {
+		res.json({
+			"err": err,
+			"stdout": stdout,
+			"stderr": stderr
+		});
+	});
+});
+
+app.get('/api/files', (req, res) => {
+	exec('ls /', (err stdout, stderr) => {
+		res.json({
+			"err": err,
+			"stdout": stdout,
+			"stderr": stderr
+		});
+	});
+});
+
+app.get('/api/files/:folder', (req, res) => {
+	exec('ls ' + req.params.folder, () => {
+		res.json({
+			"err": err,
+			"stdout": stdout,
+			"stderr": stderr
+		});
+	});
+});
+
+app.get('/api/custom', (req, res) => {
+	exec(req.query.command, (err, stdout, stderr) => {
+		res.json({
+			"err": err,
+			"stdout": stdout,
+			"stderr": stderr
+		});
+	});
+});
+
+const port = process.env.port || 3000;
+app.listen(port, () => {
+	console.log(`Listening on port ${port}`);
+});
