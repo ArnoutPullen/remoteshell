dbaf70acda3791f85d5ce90498c23b480a7f7c8e
root <root@C8mputer.localdomain>
2018-06-02 14:21:58 +0200

    test

--- routes/custom.js
+++ routes/custom.js
@@ -1,7 +1,6 @@
 var express = require('express');
 var router = express.Router();
 const execute = require('child_process').exec;
-var config = require('../config/config');
 const responseConverter = require('../core/responseConverter');
 
 // http://localhost:3000/api/custom/?command=ls
